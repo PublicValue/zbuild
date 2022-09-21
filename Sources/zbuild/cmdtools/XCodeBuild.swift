190564044a165c318d68fc3e3307f5fb08006e3b
Julian Kalinowski <Julian.Kalinowski@dataport.de>
2022-09-21 20:33:19 +0200

    fix: test process handling

--- Sources/zbuild/cmdtools/XCodeBuild.swift
+++ Sources/zbuild/cmdtools/XCodeBuild.swift
@@ -17,31 +17,74 @@ class XCodeBuild {
     func execute(arguments: [String]) async throws {
         let xcbeautify = Command.findInPath(withName: "xcbeautify")
 
-        let xcodebuild = Command.findInPath(withName: "xcodebuild")?
-                .addArguments(arguments)
-                .setCWD(FilePath(workingDir))
+//        let xcodebuild = Command.findInPath(withName: "xcodebuild")?
+//                .addArguments(arguments)
+//                .setCWD(FilePath(workingDir))
 
-        let exitStatus:ExitStatus?
-        if let xcbeautify = xcbeautify, let xcodebuild = xcodebuild, false {
-            // pipe output through xcbeautify
-            print("Found xcbeautify, piping output...")
-            let xcodebuildProc = try xcodebuild.setStdout(.pipe).spawn()
-
-            try xcbeautify
-                    .setStdin(.pipe(from: xcodebuildProc.stdout))
-                    .wait()
-            exitStatus = try xcodebuildProc.wait()
-        } else {
-            print("Could not find xcbeautify. For more beautiful output, please install xcbeautify.")
-            exitStatus = try xcodebuild?.wait()
+        guard let xcodePath = Command.findInPath(withName: "xcodebuild") else {
+            throw ZBuildError("Could not find xcodebuild in path")
         }
 
+        let xcodebuild = Process()
+        let url = xcodePath.executablePath.url
+        xcodebuild.executableURL = url
+        xcodebuild.currentDirectoryPath = workingDir
+        xcodebuild.arguments = arguments
+        let outPipe = Pipe()
+        xcodebuild.standardOutput = outPipe
+        let errPipe = Pipe()
+        xcodebuild.standardError = errPipe
 
-        if let exitStatus = exitStatus {
-            if !exitStatus.terminatedSuccessfully {
-                throw ZBuildError(message: "Command failed with code: \(exitStatus.exitCode.map{String($0)} ?? "Unknown")")
+        let outputHandle = outPipe.fileHandleForReading
+        outputHandle.waitForDataInBackgroundAndNotify()
+
+        let handler: (FileHandle) -> () = { pipe in
+
+            guard let currentOutput = String(data: pipe.availableData, encoding: .utf8) else {
+                print("Error decoding data: \(pipe.availableData)")
+                return
+            }
+
+            guard !currentOutput.isEmpty else {
+                return
+            }
+//            output = output + currentOutput + "\n"
+
+            DispatchQueue.main.async {
+                print(currentOutput)
             }
         }
+        outputHandle.readabilityHandler = handler
+
+        let errHandle = errPipe.fileHandleForReading
+        errHandle.waitForDataInBackgroundAndNotify()
+
+        errHandle.readabilityHandler = handler
+
+        try xcodebuild.run()
+        xcodebuild.waitUntilExit()
+
+//        let exitStatus:ExitStatus?
+//        if let xcbeautify = xcbeautify, let xcodebuild = xcodebuild {
+//            // pipe output through xcbeautify
+//            print("Found xcbeautify, piping output...")
+//            let xcodebuildProc = try xcodebuild.setStdout(.pipe).spawn()
+//
+//            try xcbeautify
+//                    .setStdin(.pipe(from: xcodebuildProc.stdout))
+//                    .wait()
+//            exitStatus = try xcodebuildProc.wait()
+//        } else {
+//            print("Could not find xcbeautify. For more beautiful output, please install xcbeautify.")
+//            exitStatus = try xcodebuild?.wait()
+//        }
+//
+//
+//        if let exitStatus = exitStatus {
+//            if !exitStatus.terminatedSuccessfully {
+//                throw ZBuildError(message: "Command failed with code: \(exitStatus.exitCode.map{String($0)} ?? "Unknown")")
+//            }
+//        }
     }
 
     func executeWithResult(arguments: [String]) async throws -> ChildProcess<UnspecifiedInputSource, PipeOutputDestination, UnspecifiedOutputDestination>.OutputHandle.AsyncLines {
